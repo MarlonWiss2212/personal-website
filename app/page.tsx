737b3819089cf518e8255a1551e89cc0ae778d57
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-10-17 15:45:32 +0200

    fix compile error

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="h-full flex flex-col items-start justify-center gap-4 lg:pr-48">
         <h2 className='text-md font-bold text-gray-400'>Hi, my name is</h2>
         <h1 className='text-5xl font-bold'>Marlon Wißkirchen</h1>
-        <h2 className='text-5xl text-gray-500 font-bold'>I'm a software engineer</h2>
+        <h2 className='text-5xl text-gray-500 font-bold'>I&apos;m a software engineer</h2>
         <p className='text-gray-500'>
           I am a software engineer from Rhineland Palatinate, DE. 
         </p>
