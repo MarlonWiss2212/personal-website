58b123baa35a5521f48d645e3e80e26aec6d8e11
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-11-07 19:14:08 +0100

    CV text revised

--- components/resume/timeline.tsx
+++ components/resume/timeline.tsx
@@ -7,11 +7,11 @@ export default function Timeline() {
 
   return (
     <div className="flex flex-col flex-grow overflow-y-scroll text-white md:px-10 cursor-default lg:pb-14">
-      <TimelineDateBox year="2019-2020" text={t("box1.description")} title={t("box1.title")} onRight={true}></TimelineDateBox>
+      <TimelineDateBox year="2017-2021" text={t("box1.description")} title={t("box1.title")} onRight={true}></TimelineDateBox>
       <TimelineDateBox year="2020" month={tMonth("january")} text={t("box2.description")} title={t("box2.title")} onRight={false}></TimelineDateBox>
       <TimelineDateBox year="2021" month={tMonth("july")} text={t("box3.description")} title={t("box3.title")} onRight={true}></TimelineDateBox>
       <TimelineDateBox year="2021" month={tMonth("august")} text={t("box4.description")} title={t("box4.title")} onRight={false}></TimelineDateBox>
-      <TimelineDateBox year="2023" month={tMonth("august")} text={t("box5.description")} title={t("box5.title")} onRight={true}></TimelineDateBox>
+      <TimelineDateBox year="2022-2023" text={t("box5.description")} title={t("box5.title")} onRight={true}></TimelineDateBox>
     </div>
   )
 }
