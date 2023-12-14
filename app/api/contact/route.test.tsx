38b40507b43c626f1194ca373850086ec9499952
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-12-14 23:56:40 +0100

    add test file for api routes

---
+++ app/api/contact/route.test.tsx
@@ -0,0 +1,13 @@
+import { POST } from "./route"
+import { render, screen } from "@testing-library/react"
+import {SendMailType} from "@/types/send-mail-type";
+
+describe("Send Email Route", () => {
+  it("should return 500 if there is no validation", async () => {
+    /**const data: SendMailType = {message: "", title: "", sendFromEmail: "", phoneNumber: ""}
+    let request: Request = new Request(data);
+    const response = await POST(request)
+    const image = await screen.findByTestId(`appStoreImage${appStoreLink}`)
+    expect(image).toBeVisible()*/
+  })
+})
