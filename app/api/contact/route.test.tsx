243003ce7a237660f5e7f193591ff9227b6de583
Marlon Wißkirchen <marlon.wisskirchen@outlook.de>
2023-12-15 15:40:50 +0100

    write unit test for api route

--- app/api/contact/route.test.tsx
+++ app/api/contact/route.test.tsx
@@ -1,13 +1,20 @@
 import { POST } from "./route"
-import { render, screen } from "@testing-library/react"
-import {SendMailType} from "@/types/send-mail-type";
+import { SendMailType } from "@/types/send-mail-type";
+import { NextRequest } from "next/server";
+import { createMocks } from "node-mocks-http"
+import { expect } from "vitest";
 
 describe("Send Email Route", () => {
-  it("should return 500 if there is no validation", async () => {
-    /**const data: SendMailType = {message: "", title: "", sendFromEmail: "", phoneNumber: ""}
-    let request: Request = new Request(data);
-    const response = await POST(request)
-    const image = await screen.findByTestId(`appStoreImage${appStoreLink}`)
-    expect(image).toBeVisible()*/
+  it("should return 500 if email is wrong", async () => {
+    const data: SendMailType = {
+      message: "Test Message",
+      title: "Test Title",
+      sendFromEmail: "marlon",
+      phoneNumber: "136686787"
+    }
+    const { req } = createMocks({ body: data, method: "POST" })
+    const response = await POST(req)
+    
+    expect(response.status).toBe(500)
   })
 })
--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,28 +1,35 @@
 import nodemailer from 'nodemailer'
 import {SendMailType} from "@/types/send-mail-type";
 import {NextResponse} from "next/server";
+import {EmailUseCases} from "./../../../domain/usecases/email.usecases";
 
 export async function POST(request: Request) {
-    try {
-        const emailData: SendMailType = await request.json()
-        const transporter = nodemailer.createTransport({
-            host: process.env.EMAIL_SERVICE,
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
-            },
-        })
-        const response = await transporter.sendMail({
-            from: process.env.EMAIL_SEND_FROM,
-            to: process.env.EMAIL_SEND_TO,
-            subject: emailData.title,
-            text: `${emailData.sendFromEmail}, ${emailData.phoneNumber}: ${emailData.message}`,
-            html: `${emailData.sendFromEmail}, ${emailData.phoneNumber}: ${emailData.message}`,
-        })
-        return NextResponse.json({ status: 200, data: response })
-    } catch (e) {
-        return NextResponse.json({ status: 500 })
+  const emailUseCases = new EmailUseCases()
+  try {
+    const emailData: SendMailType = await request.json()
+
+    if(!emailUseCases.checkIfEmailIsValid(emailData.sendFromEmail)) {
+      return NextResponse.json(null, { status: 500 })
     }
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_SERVICE,
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+        },
+    })
+    const response = await transporter.sendMail({
+      from: process.env.EMAIL_SEND_FROM,
+      to: process.env.EMAIL_SEND_TO,
+      subject: emailData.title,
+      text: `${emailData.sendFromEmail}, ${emailData.phoneNumber}: ${emailData.message}`,
+      html: `${emailData.sendFromEmail}, ${emailData.phoneNumber}: ${emailData.message}`,
+    })
+    return NextResponse.json({ ...response }, { status: 200 })
+  } catch (e) {
+    return NextResponse.json(null, { status: 500 })
+  }
 }
